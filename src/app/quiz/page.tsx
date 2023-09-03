32862f517364935ab25c4ff4c61fbadfb8888c34
Abiola Oyewale <abiol4001@gmail.com>
2023-09-03 10:27:58 +0100

    Quiz Creation page completed

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { getAuthSession } from '@/lib/nextauth'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+import { quizCreationSchema } from "@/schemas/form/quiz"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form'
+import CreateQuiz from '@/components/quiz/CreateQuiz'
 
 type Props = {}
 
-const QuizPage = (props: Props) => {
-  return (
-    <div>QuizPage</div>
-  )
+export const metadata = {
+    title: "Dashboard | AIgnite"
+}
+
+const QuizPage = async (props: Props) => {
+    
+    const session = await getAuthSession()
+    if (!session?.user) {
+        return redirect("/")
+    }
+
+    return (
+        <main className='pt-32 mx-auto max-w-7xl'>
+            <CreateQuiz />
+        </main>
+    )
 }
 
 export default QuizPage
